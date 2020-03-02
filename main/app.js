4f5174832c5cab5b81fdc88ab9045f5028e567de
716green <bob@bbass.co>
2020-03-01 21:23:06 -0500

    added new html and css (in-file)

--- main/app.js
+++
@@ -1,4 +0,0 @@
-// not in use
-// Set alert so that I can see if/when it runs
-alert('app.js has run')
-console.log('app.js has run')
---
+++ main/xapp.js
@@ -0,0 +1,9 @@
+// not in use
+let sampleArgument
+
+function sampleFunction(sampleArgument) {
+    sampleArgument = 'It Worked!'
+    alert(sampleArgument)
+    console.log('app.js has run')
+    return sampleArgument
+}
