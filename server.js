abc59c356e1ce9b935842c6d9834ecfe9a07fc82
716green <bob@bbass.co>
2020-03-01 21:24:47 -0500

    fixed html and css

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-
 // 'http' can be used instead of express and vice versa
 const http = require('http')
-// fs is required to parse HTML files
+    // fs is required to parse HTML files
 const fs = require('fs')
 const port = 3000
 
@@ -9,11 +8,11 @@ const server = http.createServer(function(req, res) {
     // 200 means 'good'
     res.writeHead(200, { 'Content-Type': 'text/html' })
     fs.readFile('./main/index.html', function(error, data) {
-    // fs.readFile('index.html', function(error, data) {
+        // fs.readFile('index.html', function(error, data) {
         if (error) {
             // 404 = not found
             res.writeHead(404)
-            // Usually this will render a 404 page
+                // Usually this will render a 404 page
             res.write('Error: File Not Found')
         } else {
             res.write(data)
